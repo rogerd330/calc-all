f132de9bf7d8f0ad17561b9ff3e78a8c6458450e
Roger Dickey <rdickey@whytespyder.com>
2021-02-05 18:58:15 -0600

    Use Jest setup & teardown to use a single instance of the Calculator class in all tests.

--- scripts/calc.test.js
+++ scripts/calc.test.js
@@ -1,27 +1,36 @@
 const { test, expect } = require("@jest/globals");
 import { Calculator } from "./calc.js"
 
+// Declare the 'calc' variable in the global scope.
+let calc;
+
+// Before any test runs, initialize our 'calc' object 
+// so it can be referenced in all the tests.
+beforeAll(() => {
+    calc = new Calculator();
+});
+
+// Clean up after all of our tests have run.
+afterAll(() => {
+    calc = null;
+});
+
 test("can add 1 + 2?", () => {
-    let calc = new Calculator();
     expect(calc.add(1,2)).toBe(3);
 });
 
 test("can't add nulls", () => {
-    let calc = new Calculator();
     expect(calc.add(null,null)).toBe(0);
 });
 
 test("can subtract 10 from 20?", () => {
-    let calc = new Calculator();
     expect(calc.subtract(20,10)).toBe(10);
 });
 
 test("can multiply 10 by 10?", () => {
-    let calc = new Calculator();
     expect(calc.multiply(10, 10)).toBe(100);
 });
 
 test("can divide 10 by 2?", () => {
-    let calc = new Calculator();
     expect(calc.divide(10, 2)).toBe(5);
 });
